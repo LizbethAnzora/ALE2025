6471da60badcad5f4c066805c8a0ca8632560c41
Eneida Lizbeth Anzora Monge <anzoraeneida@outlook.com>
2025-06-07 18:06:31 -0600

    Se creo el paquete dominio y la clase Usuario

---
+++ src/main/java/ale2025/dominio/Usuario.java
@@ -0,0 +1,76 @@
+package ale2025.dominio;
+
+public class Usuario {
+    private int id;
+    private String nombre;
+    private String passwordHash;
+    private String correoElectronico ;
+    private byte Estado;
+
+    public Usuario() {
+    }
+
+    public Usuario(int id, String nombre, String passwordHash, String correoElectronico, byte estado) {
+        this.id = id;
+        this.nombre = nombre;
+        this.passwordHash = passwordHash;
+        this.correoElectronico = correoElectronico;
+        Estado = estado;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public byte getEstado() {
+        return Estado;
+    }
+
+    public void setEstado(byte estado) {
+        Estado = estado;
+    }
+
+    public String getStrEstado(){
+        String str="";
+        switch (Estado){
+            case 1:
+                str = "ACTIVO";
+                break;
+            case 2:
+                str = "INACTIVO";
+                break;
+            default:
+                str = "";
+        }
+        return str;
+    }
+}
+
