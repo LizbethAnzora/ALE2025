8cb2da95ba5a18b24d00038a4faf049b761cd54d
Esmeralda Gissel Magaña Sánchez <ms25001@esfe.agape.edu.sv>
2025-06-09 15:21:16 -0600

    Descargue los cambios actualizados y revise el ódigo

--- src/main/java/ale2025/persistencia/ConnectionManager.java
+++ src/main/java/ale2025/persistencia/ConnectionManager.java
@@ -22,7 +22,7 @@ public class ConnectionManager {
      * - user=dev : Nombre de usuario para la autenticación.
      * - password=dev : Contraseña para la autenticación.
      */
-    private static final String STR_CONNECTION = "jdbc:sqlserver://DESKTOP-P7F1BRV\\SQLEXPRESS;" +
+    private static final String STR_CONNECTION = "jdbc:sqlserver://ANZORAENEIDAPC\\SQLEXPRESS;" +
             "encrypt=true; " +
             "database=ClinicaSaludTotal; " +
             "trustServerCertificate=true;" +
