4cd957f4246093d3c5010205af0afc7b6aac41ac
Esmeralda Gissel Magaña Sanchez <ms25001@esfe.agape.edu.sv>
2025-06-14 21:57:59 -0600

    Se creo y se dio diseño a los Form de Horario

---
+++ src/main/java/ale2025/presentacion/HorarioReadingForm.java
@@ -0,0 +1,12 @@
+package ale2025.presentacion;
+
+import javax.swing.*;
+
+public class HorarioReadingForm {
+    private JTextField txtDiaSemana;
+    private JButton btnCreate;
+    private JTable tableHorarios;
+    private JButton btnDelete;
+    private JButton btnUpdate;
+    private JPanel mainPanel;
+}
---
+++ src/main/java/ale2025/presentacion/HorarioWriteForm.java
@@ -0,0 +1,13 @@
+package ale2025.presentacion;
+
+import javax.swing.*;
+
+public class HorarioWriteForm {
+    private JComboBox cbMedicoId;
+    private JButton btnOk;
+    private JButton btnCancel;
+    private JPanel mainPanel;
+    private JTextField txtDiaSemana;
+    private JPanel panelHoraInicio;
+    private JPanel panelHoraFin;
+}
