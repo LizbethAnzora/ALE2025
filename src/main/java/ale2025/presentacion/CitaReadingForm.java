758edb51c775b9000d3c064d2407be095dda9ff3
Esmeralda Gissel Magaña Sanchez <ms25001@esfe.agape.edu.sv>
2025-06-14 23:20:49 -0600

    Se creo y se dio diseño a los Form de Cita

---
+++ src/main/java/ale2025/presentacion/CitaReadingForm.java
@@ -0,0 +1,16 @@
+package ale2025.presentacion;
+
+import javax.swing.*;
+
+public class CitaReadingForm {
+    private JPanel mainPanel;
+    private JButton btnCreate;
+    private JPanel panelFechaCita;
+    private JTable table1;
+    private JButton btnUpdate;
+    private JButton btnDelete;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
---
+++ src/main/java/ale2025/presentacion/CitaWriteForm.java
@@ -0,0 +1,13 @@
+package ale2025.presentacion;
+
+import javax.swing.*;
+
+public class CitaWriteForm {
+    private JComboBox cbMedicoId;
+    private JTextField txtFechaCita;
+    private JTextField txtCostoConsulta;
+    private JButton btnOk;
+    private JButton btnCancel;
+    private JPanel mainPanel;
+    private JComboBox cbPacienteId;
+}
